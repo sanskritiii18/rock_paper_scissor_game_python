d086137372d457e3fcfa3062edd7e4e27005169b
SANSKRITI VERMA <sanskritiverma.1807@gmail.com>
2024-10-12 23:46:35 +0530

    names added

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import random
-#this is a basic rock,paper,scissor game
-#functions and parameters used
-#if else condition is used
-count1 =0
-count2= 0
+# this is a basic rock,paper,scissor game
+# functions and parameters used
+# if else condition is used
+
 #WINNING CONDITIONSā
-def option(c, d, player1, player2, count2=None, count1=None):
-        if player1 == player2:
-            print("its a tie")
-        elif player1 =="rock" and player2=="paper":
-            count2 +=1
-        elif player1 =="rock" and player2=="scissor":
-            count1 +=1
-        elif player1 =="scissor" and player2=="rock":
-            count2 +=1
-        elif player1 =="scissor" and player2 =="paper":
-            count1 +=1
-        elif player1=="paper" and player2=="scissor":
-            count2 +=1
-        elif player1 =="paper" and player2=="rock":
-            count1 +=1
-        else:
-            print("enter a valid option")
-        return count1,count2
+def option(c, d,player1,player2):
+    if player1 == player2:
+        print("its a tie")
+    elif player1 =="rock" and player2=="paper":
+        print(f"{d} wins")
+    elif player1 =="rock" and player2=="scissor":
+        print(f"{c} wins")
+    elif player1 =="scissor" and player2=="rock":
+        print(f"{d} wins")
+    elif player1 =="scissor" and player2 =="paper":
+        print(f"{c} wins")
+    elif player1=="paper" and player2=="scissor":
+        print(f"{d} wins")
+    elif player1 =="paper" and player2=="rock":
+        print(f"{c} wins")
+    else:
+        print("enter a valid option")
 
 #if both selected  players are users
 def bothplayers(a,b):
@@ -33,41 +31,43 @@ def bothplayers(a,b):
     value1 = input()
     print("player 2 enter from options")
     value2 = input()
-    option(a,b,value1,value2,count1,count2)
-    return count1,count2
+    option(a,b,value1,value2)
+
 #if its computer vs player
 def playervscomp(a,b):
       if player1 == "player":
-            options = ('rock', 'paper', 'scissor')
-            print("options: rock , paper , scissor")
-            print("player1 enter options")
-            value1 = input()
-            computer = random.choice(options)
-            print(computer)
-            value2 = computer
-            option(a,b,value1,value2)
+        options = ('rock', 'paper', 'scissor')
+        print("options: rock , paper , scissor")
+        print("player1 enter options")
+        value1 = input()
+        computer = random.choice(options)
+        print(computer)
+        value2 = computer
+        option(a,b,value1,value2)
       else:
-            options = ('rock', 'paper', 'scissor')
-            computer = random.choice(options)
-            print(f"computer:{computer}")
-            value1 = computer
-            options = ('rock', 'paper', 'scissor')
-            print("options: rock , paper , scissor")
-            print("player2 enter options:")
-            value2 = input()
-            option(a,b,value1,value2)
+        options = ('rock', 'paper', 'scissor')
 
+        computer = random.choice(options)
+        print(f"computer:{computer}")
+        value1 = computer
+        options = ('rock', 'paper', 'scissor')
+        print("options: rock , paper , scissor")
+        print("player2 enter options:")
+        value2 = input()
+        option(a,b,value1,value2)
 
 
 #if both selected players are computer
 def compvscomp(a,b):
     print("computer vs computer")
     options = ('rock', 'paper', 'scissor')
+
     value1 = random.choice(options)
     value2 = random.choice(options)
     print(value1)
     print(value2)
     option(a,b,value1,value2)
+
 
 
 #number of players can be two
@@ -88,23 +88,19 @@ if (player1=="player") and (player2 =="player"):
     print("You entered:", name1)
     name2 = input("Enter your name: ")
     print("You entered:", name2)
-    for x in range(round):
-     bothplayers(name1,name2)
-     if count1>count2:
-         print(f"{name1} wins")
-     else:
-         print(f"{name2} wins")
+    bothplayers(name1,name2)
 elif (player1 =="player") or (player2 =="player"):
     if player1=="player":
-        name1 = input("Enter your name: ")
-        print("You entered:", name1)
-        playervscomp(name1,"computer")
+       name1 = input("Enter your name: ")
+       print("You entered:", name1)
+       playervscomp(name1, "computer")
     else:
-        name1 = input("Enter your name: ")
-        print("You entered:", name1)
-        playervscomp("computer",name1)
+       name1 = input("Enter your name: ")
+       print("You entered:", name1)
+       playervscomp("computer",name1)
+
 elif (player1=="computer") and (player2=="computer"):
- compvscomp("computer1","computer2")
+       playervscomp("computer1","computer2")
 else:
  print("enter valid options")
 
